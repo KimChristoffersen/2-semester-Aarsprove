9b5145484244f50b66b77538a31e1554ca2f72e3
Kim <10434639@ucn.dk>
2022-11-17 12:22:34 +0100

    Update BookingController.java

--- src/controller/BookingController.java
+++ src/controller/BookingController.java
@@ -1,17 +1,26 @@
 package controller;
 
+import java.util.List;
+
 import model.*;
 
 public class BookingController {
 
 	private Booking currentBoooking;
-	// private ShootingRangeDBIF shootingRangeDB;
-	
-//	public Booking createBooking(Customer customer) {
-//		currentBoooking = new Booking();
-//		currentBooking.addCustomer(customer);
-//		return booking;
-//	}
-	
+	// private CustomerDBIF customerDB;
+	// private BookingDBIF bookingDB;
 
+	// public Booking createBooking(Customer customer) {
+	// currentBoooking = new Booking();
+	// currentBooking.addCustomer(customer);
+	// return booking;
+	// }
+
+	// public List<Integer> getAvailableShootingRanges(LocalDate date, int time){
+	// return shootingRangeIds = bookingDB.getAvailableShootingRangeIds();
+	// }
+
+	// public List<Integer> getAvailableInstructors(LocalDate date, int time){
+	// return availableInstructors = bookingDB.getAvailableInstructors();
+	// }
 }

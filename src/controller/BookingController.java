3fc571b56e73cc5ee7c2e706f4bacd31a741413c
Kim <10434639@ucn.dk>
2022-11-17 09:28:09 +0100

    Controllers

---
+++ src/controller/BookingController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import model.*;
+
+public class BookingController {
+
+	private Booking currentBoooking;
+	// private ShootingRangeDBIF shootingRangeDB;
+	
+//	public Booking createBooking(Customer customer) {
+//		currentBoooking = new Booking();
+//		currentBooking.addCustomer(customer);
+//		return booking;
+//	}
+	
+
+}
